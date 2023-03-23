c29b8fd15ea7476a196bac37a912feb9768a2549
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2023-03-23 09:01:39 +0000

    Update Skilaverkefni 3.sql

--- Skilaverkefni 3/Skilaverkefni 3.sql
+++ Skilaverkefni 3/Skilaverkefni 3.sql
@@ -61,17 +61,25 @@ CALL deleteCourse('TEST101');
 -- Courses -- -- Courses -- -- Courses -- -- Courses -- -- Courses -- -- Courses -- -- Courses --
 -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- 
 -- create --
+drop procedure if exists CreateTrackCourse;
 DELIMITER $$
-CREATE PROCEDURE create_track_course(IN track_id INT, IN course_number CHAR(15), IN semester TINYINT UNSIGNED, IN mandatory TINYINT UNSIGNED)
+CREATE PROCEDURE createTrackCourse(
+  IN track_id INT,
+  IN course_number CHAR(15),
+  IN semester TINYINT UNSIGNED,
+  IN mandatory TINYINT UNSIGNED
+)
 BEGIN
   INSERT INTO TrackCourses (trackID, courseNumber, semester, mandatory)
   VALUES (track_id, course_number, semester, mandatory);
 END $$
 DELIMITER ;
+SELECT * FROM TrackCourses;
+
 
 -- Read --
 DELIMITER $$
-CREATE PROCEDURE read_track_course(IN track_id INT, IN course_number CHAR(15))
+CREATE PROCEDURE readTrackCourse(IN track_id INT, IN course_number CHAR(15))
 BEGIN
   SELECT *
   FROM TrackCourses
@@ -80,7 +88,7 @@ END $$
 DELIMITER ;
 -- Update --
 DELIMITER $$
-CREATE PROCEDURE update_track_course(IN track_id INT, IN course_number CHAR(15), IN semester TINYINT UNSIGNED, IN mandatory TINYINT UNSIGNED)
+CREATE PROCEDURE updateTrackCourse(IN track_id INT, IN course_number CHAR(15), IN semester TINYINT UNSIGNED, IN mandatory TINYINT UNSIGNED)
 BEGIN
   UPDATE TrackCourses
   SET semester = semester, mandatory = mandatory
@@ -89,7 +97,7 @@ END$$
 DELIMITER ;
 -- Delete --
 DELIMITER $$
-CREATE PROCEDURE DeleteTrackCourse (
+CREATE PROCEDURE deleteTrackCourse (
   IN p_trackID INT,
   IN p_courseNumber CHAR(15)
 )
@@ -97,13 +105,73 @@ BEGIN
   DELETE FROM TrackCourses
   WHERE trackID = p_trackID AND courseNumber = p_courseNumber;
 END $$
-
 DELIMITER ;
 
-CALL create_track_course(1, 'CSCI101', 1, 1);
-CALL read_track_course(1, 'CSCI101');
-CALL update_track_course(1, 'CSCI101', 2, 0);
-CALL DeleteTrackCourse(1, 'ABC123');
-
-
+CALL createTrackCourse(9, 'TEST201', 1, 1);
+CALL readTrackCourse(1, 'TEST201');
+CALL updateTrackCourse(1, 'TEST201', 2, 0);
+CALL DeleteTrackCourse(1, 'BLA123');
 -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- -- TrackCourses -- 
+-- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- 
+
+-- Create
+DELIMITER $$
+CREATE PROCEDURE createSemester (
+  IN semesterName CHAR(10),
+  IN semesterStarts DATE,
+  IN semesterEnds DATE,
+  IN academicYear CHAR(10)
+)
+BEGIN
+  INSERT INTO Semesters (semesterName, semesterStarts, semesterEnds, academicYear)
+  VALUES (semesterName, semesterStarts, semesterEnds, academicYear);
+END$$
+DELIMITER ;
+
+-- Read --
+DELIMITER $$
+CREATE PROCEDURE read_semester (
+  IN semesterID INT
+)
+BEGIN
+  SELECT *
+  FROM Semesters
+  WHERE semesterID = semesterID;
+END;
+DELIMITER ;
+
+-- Update --
+DELIMITER $$
+CREATE PROCEDURE updateSemester (
+  IN semesterID INT,
+  IN semesterName CHAR(10),
+  IN semesterStarts DATE,
+  IN semesterEnds DATE,
+  IN academicYear CHAR(10)
+)
+BEGIN
+  UPDATE Semesters
+  SET semesterName = semesterName,
+      semesterStarts = semesterStarts,
+      semesterEnds = semesterEnds,
+      academicYear = academicYear
+  WHERE semesterID = semesterID;
+END;
+DELIMITER ;
+
+-- Delete --
+DELIMITER $$
+CREATE PROCEDURE deleteSemester (
+  IN semesterID INT
+)
+BEGIN
+  DELETE FROM Semesters
+  WHERE semesterID = semesterID;
+END;
+DELIMITER ;
+
+CALL createSemester('Spring', '2023-01-10', '2023-05-10', '2022-2023');
+CALL readSemester(1);
+CALL updateSemester(1, 'Summer', '2023-06-01', '2023-08-31', '2022-2023');
+CALL deleteSemester(1);
+-- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- -- Semesters -- 
